a416c69ee6d639334afa502be989ae909f345158
Raul Galindo <raul.galindo@xing.com>
2016-09-07 01:30:15 +0200

    allow flappy to die in peace

--- main.js
+++ main.js
@@ -52,16 +52,37 @@ var mainState = {
             this.bird.angle += 1;
 
         // kill flappy if he hits a pipe line
-        game.physics.arcade.overlap(
-            this.bird, this.pipes, this.restartGame, null, this);
-        },
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+    },
+
     // Make the bird jump
     jump: function() {
+        // if flappy is dead, dont jump at all
+        if (this.bird.alive == false)
+            return;
         // Add a vertical velocity to the bird
         this.bird.body.velocity.y = -300;
 
         // animate jumping bird
         game.add.tween(this.bird).to({angle: -20}, 100).start();
+    },
+
+    hitPipe: function() {
+        // If the bird has already hit a pipe, do nothing
+        // It means the bird is already falling off the screen
+        if (this.bird.alive == false)
+            return;
+
+        // Set the alive property of the bird to false
+        this.bird.alive = false;
+
+        // Prevent new pipes from appearing
+        game.time.events.remove(this.timer);
+
+        // Go through all the pipes, and stop their movement
+        this.pipes.forEach(function(p){
+            p.body.velocity.x = 0;
+        }, this);
     },
 
     addOnePipe: function(x, y) {
